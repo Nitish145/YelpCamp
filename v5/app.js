8a2f7d0ac6d1376bf0be0d2b01bdbab3ebebb7a5
Nitish Aggarwal <royalnitish21@gmail.com>
2019-08-07 00:07:59 +0530

    feat: Add User logout implementation and isLoggedIn middleware

--- v5/app.js
+++ v5/app.js
@@ -79,7 +79,7 @@ app.get("/campgrounds/:id", function (req, res) {
     })
 })
 
-app.get("/campgrounds/:id/comments/new", function (req, res) {
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function (req, res) {
     Campground.findById(req.params.id, function (err, foundCampground) {
         if (err) {
             console.log("Something Went Wrong!!");
@@ -90,7 +90,7 @@ app.get("/campgrounds/:id/comments/new", function (req, res) {
     });
 });
 
-app.post("campgrounds/:id/comments", function (req, res) {
+app.post("campgrounds/:id/comments", isLoggedIn , function (req, res) {
     Campground.findById(req.body.id, function (err, campground) {
         if (err) {
             console.log(err);
@@ -140,6 +140,18 @@ app.post("/login", passport.authenticate("local", {
 }), function (req, res) {
 
 })
+
+app.get("/logout" , function(req, res){
+    req.logOut();
+    res.redirect("/campgrounds");
+})
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(3000, function () {
     console.log("The YelpCamp Server has started");
